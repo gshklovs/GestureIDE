fa0abc13a1786a7fab94a2687238d6359bdfe9ef
Gregory Shklovski <gshklovs@uci.edu>
2024-10-08 01:12:27 -0700

    added cursor functionality, hand overlay, and ide

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,68 @@
-import { useRef } from "react";
+import React, { useRef, useEffect } from "react";
+import Monaco from "@monaco-editor/react";
 import useGestureRecognition from "./components/hands-capture/hooks";
+import "./App.css";
+import Cursor from "./components/Cursor";
 
 function App() {
-  const videoElement = useRef<any>()
-  const canvasEl = useRef<any>()
-  const { maxVideoWidth, maxVideoHeight } = useGestureRecognition({
+  const videoElement = useRef<any>();
+  const canvasEl = useRef<any>();
+  const editorRef = useRef<any>();
+
+  const { landmarks, maxVideoWidth, maxVideoHeight } = useGestureRecognition({
     videoElement,
-    canvasEl
+    canvasEl,
   });
 
+  // Function to capture the editor instance on mount
+  function handleEditorDidMount(editor: any, monaco: any) {
+    editorRef.current = editor;
+  }
+
+  useEffect(() => {
+    // Check if the editor instance is available and if landmarks exist
+    if (editorRef.current && landmarks) {
+      // Set the editor's value to the stringified landmarks data
+      editorRef.current.setValue(
+        JSON.stringify(
+          landmarks
+            ? landmarks[0]
+              ? landmarks[0][8]["z"]
+              : "All landmarks"
+            : []
+        )
+      );
+    }
+  }, [landmarks]);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
+    <div style={{}}>
+      {landmarks && landmarks[0] && <Cursor landmarks={landmarks[0]} />}
+      <Monaco
+        height="50vh" // Editor height
+        width="80vw" // Editor width
+        theme="vs-dark" // Theme options: "vs-light", "vs-dark", "hc-black"
+        defaultLanguage="python" // Set the language of the editor
+        defaultValue="// Start coding here"
+        onMount={handleEditorDidMount} // onMount handler to capture the editor instance
+        options={{
+          fontSize: 16,
+          minimap: { enabled: true },
+          lineNumbers: "on",
+          automaticLayout: true,
+          scrollBeyondLastLine: false,
+          wordWrap: "on",
+        }}
+      />
       <video
-        style={{ display: 'none' }}
-        className='video'
+        style={{ display: "none" }}
+        className="video"
         playsInline
         ref={videoElement}
       />
-      <canvas 
-        ref={canvasEl} 
-        width={maxVideoWidth} 
-        height={maxVideoHeight} 
-        />
+      <canvas ref={canvasEl} width={maxVideoWidth} height={maxVideoHeight} />
     </div>
   );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true, // Ensure source maps are enabled
+  },
+  // Optionally, you can also enable source maps for development
   server: {
-    port: 3000
+    sourcemap: true,
+    port: 3000,
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/__tests__/setup.ts", // assuming the test folder is in the root of our project
   },
   plugins: [react()],
-})
+});
